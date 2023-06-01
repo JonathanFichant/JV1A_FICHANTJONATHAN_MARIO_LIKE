b400563b80f35c6a4caf0e2471faf0b2850f3607
JonathanFichant <118370145+JonathanFichant@users.noreply.github.com>
2023-06-01 10:40:29 +0200

    Add files via upload

---
+++ tiled/tileset2Zelda.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="tileset2Zelda" tilewidth="32" tileheight="32" tilecount="600" columns="12">
+ <image source="tilesetZelda.png" width="384" height="1600"/>
+</tileset>
